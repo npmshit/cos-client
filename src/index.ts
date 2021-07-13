de3a10f9de71e58a7fea08e0cc94ff539fd59ffa
YourtionGuo <yourtion@gmail.com>
2021-07-13 13:59:32 +0800

    feat: first version

---
+++ src/index.ts
@@ -0,0 +1,279 @@
+/**
+ * @file COS Client
+ * @author Yourtion Guo <yourtion@gmail.com>
+ */
+
+import assert from "assert";
+import crypto from "crypto";
+import http, { Agent } from "http";
+import https, { RequestOptions } from "https";
+import { Readable } from "stream";
+import { extname } from "path";
+import { getType } from "mime";
+
+export interface IOption {
+  /** APPID */
+  appId: string;
+  /** 秘钥ID */
+  secretId: string;
+  /** 秘钥Key */
+  secretKey: string;
+  /** Bucket */
+  bucket: string;
+  /** 地址 */
+  region: string;
+  /** 前缀 */
+  prefix?: string;
+  /** 自定义访问地址 */
+  cdn?: string;
+  /** HTTP Agent */
+  agent?: Agent;
+}
+
+export interface IPutOption {
+  /** contentType */
+  type?: string;
+  /** 文件名（用于计算contentType） */
+  name?: string;
+  /** 文件md5 */
+  md5?: string;
+}
+
+export type METHOD = "PUT" | "GET" | "POST" | "HEAD" | "DELETE";
+
+export interface IHeader {
+  [header: string]: number | string | string[] | undefined;
+}
+
+export interface IReply {
+  code: number;
+  headers: IHeader;
+  buffer?: Buffer;
+  body?: string;
+}
+
+interface IAuth {
+  method: string;
+  key: string;
+  query?: Record<string, any>;
+  headers?: Record<string, any>;
+  expires?: number;
+}
+
+function camSafeUrlEncode(str: string) {
+  return encodeURIComponent(str)
+    .replace(/!/g, "%21")
+    .replace(/'/g, "%27")
+    .replace(/\(/g, "%28")
+    .replace(/\)/g, "%29")
+    .replace(/\*/g, "%2A");
+}
+
+function getObjectKeys(obj: Record<string, any>, forKey = false) {
+  var list = [];
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      list.push(forKey ? camSafeUrlEncode(key) : key);
+    }
+  }
+  return list.sort(function(a, b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    return a === b ? 0 : a > b ? 1 : -1;
+  });
+}
+
+function obj2str(obj: Record<string, any>) {
+  let key, val;
+  const list = [];
+  const keyList = getObjectKeys(obj);
+  for (let i = 0; i < keyList.length; i++) {
+    key = keyList[i];
+    val = obj[key] === undefined || obj[key] === null ? "" : "" + obj[key];
+    key = camSafeUrlEncode(key).toLowerCase();
+    val = camSafeUrlEncode(val) || "";
+    list.push(key + "=" + val);
+  }
+  return list.join("&");
+}
+
+export default class COSClient {
+  private appId: string;
+  private secretId: string;
+  private secretKey: string;
+  private bucket: string;
+  private region: string;
+  private prefix?: string;
+  private cdn: string;
+  private agent?: Agent;
+
+  constructor(options: IOption) {
+    assert(typeof options.appId === "string" && options.appId, "请配置 AppId");
+    assert(typeof options.secretId === "string" && options.secretId, "请配置 SecretId");
+    assert(typeof options.secretKey === "string" && options.secretKey, "请配置 SecretKey");
+    assert(typeof options.bucket === "string" && options.bucket, "请配置 bucket");
+    assert(typeof options.region === "string" && options.region, "请配置 region");
+    if (options.cdn) {
+      assert(
+        /^((^https?:)?(?:\/\/)?)([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/.test(options.cdn),
+        "cdn必须配置url(最后不需要 `/`)"
+      );
+    }
+    this.appId = options.appId;
+    this.secretId = options.secretId;
+    this.secretKey = options.secretKey;
+    this.bucket = options.bucket;
+    this.region = options.region;
+    this.prefix = options.prefix;
+    this.cdn = options.cdn || `http://${this.bucket}-${this.appId}.cos.${this.region}.myqcloud.com`;
+    this.agent = options.agent;
+  }
+
+  private getAuth(opts: IAuth) {
+    const now = Math.round(Date.now() / 1000) - 1;
+    const exp = opts.expires || now + 900;
+    const method = opts.method.toLowerCase();
+    const queryParams = { ...opts.query };
+    const headers = { ...opts.headers };
+    // 要用到的 Authorization 参数列表
+    const qSignAlgorithm = "sha1";
+    const qAk = this.secretId;
+    const qSignTime = now + ";" + exp;
+    const qKeyTime = now + ";" + exp;
+    const pathname = opts.key.indexOf("/") !== 0 ? "/" + opts.key : opts.key;
+    const qHeaderList = getObjectKeys(headers, true)
+      .join(";")
+      .toLowerCase();
+    const qUrlParamList = getObjectKeys(queryParams, true)
+      .join(";")
+      .toLowerCase();
+
+    // 签名算法说明文档：https://www.qcloud.com/document/product/436/7778
+    // 步骤一：计算 SignKey
+    const signKey = crypto
+      .createHmac("sha1", this.secretKey)
+      .update(qKeyTime)
+      .digest("hex");
+
+    // 步骤二：构成 FormatString
+    const formatString = Buffer.from([method, pathname, obj2str(queryParams), obj2str(headers), ""].join("\n"), "utf8");
+    // 步骤三：计算 StringToSign
+    const res = crypto
+      .createHash("sha1")
+      .update(formatString)
+      .digest("hex");
+    const stringToSign = ["sha1", qSignTime, res, ""].join("\n");
+    // 步骤四：计算 Signature
+    const qSignature = crypto
+      .createHmac("sha1", signKey)
+      .update(stringToSign)
+      .digest("hex");
+
+    // 步骤五：构造 Authorization
+    const authorization = [
+      "q-sign-algorithm=" + qSignAlgorithm,
+      "q-ak=" + qAk,
+      "q-sign-time=" + qSignTime,
+      "q-key-time=" + qKeyTime,
+      "q-header-list=" + qHeaderList,
+      "q-url-param-list=" + qUrlParamList,
+      "q-signature=" + qSignature,
+    ].join("&");
+    return authorization;
+  }
+
+  private getFileKey(key: string) {
+    const res = this.prefix ? this.prefix + key : key;
+    return res.replace(/^\/+/, "");
+  }
+
+  private request(params: RequestOptions, data?: Buffer | Readable, raw = false): Promise<IReply> {
+    return new Promise((resolve, reject) => {
+      const req = http.request(params, response => {
+        const buffers: any[] = [];
+        response.on("data", chunk => buffers.push(chunk));
+        response.on("end", () => {
+          const buf = Buffer.concat(buffers);
+          return resolve({
+            code: response.statusCode || -1,
+            headers: response.headers,
+            buffer: buf,
+            body: raw ? "" : buf.toString("utf8"),
+          });
+        });
+        response.on("error", err => reject(err));
+      });
+      req.on("error", err => reject(err));
+      if (Buffer.isBuffer(data)) {
+        req.end(data);
+      } else if (data && typeof data.pipe === "function") {
+        data.pipe(req);
+      } else {
+        req.end();
+      }
+    });
+  }
+
+  private requestObject(method: METHOD, key: string, data?: Buffer | Readable, raw = false, options: IPutOption = {}) {
+    const date = new Date().toUTCString();
+    const filekey = this.getFileKey(key);
+    const ext = extname(options.name || filekey);
+    const type = (method === "POST" || method === "PUT") && ext ? getType(ext.replace(".", "")) : options.type || "";
+    const hostname = `${this.bucket}-${this.appId}.cos.${this.region}.myqcloud.com`;
+    const headers = {
+      Host: hostname,
+      Date: date,
+      "Content-Type": type || "",
+    } as Record<string, any>;
+    const auth = this.getAuth({ method, key: filekey, headers });
+    headers["Authorization"] = auth;
+    const option = {
+      hostname,
+      path: encodeURI(`/${filekey}`),
+      method: method,
+      headers,
+      agent: this.agent,
+    };
+    return this.request(option, data, raw);
+  }
+
+  putObject(key: string, data: Buffer | Readable, options?: IPutOption) {
+    return this.requestObject("PUT", key, data, false, options);
+  }
+
+  getObject(key: string) {
+    return this.requestObject("GET", key, undefined, true);
+  }
+
+  deleteObject(key: string) {
+    return this.requestObject("DELETE", key);
+  }
+
+  objectMeta(key: string) {
+    return this.requestObject("HEAD", key);
+  }
+
+  headObject(key: string) {
+    return this.requestObject("HEAD", key);
+  }
+
+  // getSignUrl(key: string, ttl = 60) {
+  //   const expires = parseInt(String(new Date().getTime() / 1000 + ttl), 10);
+  //   const fielkey = this.getFileKey(key);
+  //   const query = [
+  //     `OSSAccessKeyId=${this.accessKeyId}`,
+  //     `Signature=${this.signUrl(fielkey, expires)}`,
+  //     `Expires=${expires}`,
+  //   ];
+  //   return `${this.cdn}/${fielkey}?${query.join("&")}`;
+  // }
+
+  // putObjectWithUrl(key: string, url: string, options?: IPutOption): Promise<string> {
+  //   return new Promise(resolve => {
+  //     (url.indexOf("https") === 0 ? https : http).get(url, async res => {
+  //       await this.putObject(key, res, options);
+  //       resolve(this.getSignUrl(key));
+  //     });
+  //   });
+  // }
+}
---
+++ src/test.ts
@@ -0,0 +1,140 @@
+import COSClient from "./index";
+import http from "http";
+
+const client = new COSClient({
+  appId: process.env.TEST_COS_APPID!,
+  secretId: process.env.TEST_COS_ID!,
+  secretKey: process.env.TEST_COS_KEY!,
+  bucket: process.env.TEST_COS_BUCKET!,
+  region: process.env.TEST_COS_REGION!,
+  prefix: process.env.TEST_COS_PREFEX!,
+});
+
+const clientWithAgent = new COSClient({
+  appId: process.env.TEST_COS_APPID!,
+  secretId: process.env.TEST_COS_ID!,
+  secretKey: process.env.TEST_COS_KEY!,
+  bucket: process.env.TEST_COS_BUCKET!,
+  region: process.env.TEST_COS_REGION!,
+  prefix: process.env.TEST_COS_PREFEX!,
+  agent: new http.Agent({ keepAlive: true, keepAliveMsecs: 10000 }),
+});
+
+const TEST_KEY = "OSSClient.data";
+const TEST_DATA = Date.now() + "";
+// const TEST_URL = "http://mat1.gtimg.com/pingjs/ext2020/qqindex2018/dist/img/qq_logo_2x.png";
+
+function getFile(url: string): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    return http.get(url, res => {
+      const buffers: any[] = [];
+      res.on("data", chunk => buffers.push(chunk));
+      res.on("end", () => resolve(Buffer.concat(buffers)));
+      res.on("error", err => reject(err));
+    });
+  });
+}
+
+describe("OSSClient", function() {
+  const SHARE: any = {};
+
+  beforeAll(async () => {
+    await client.deleteObject(TEST_KEY);
+  });
+
+  test("putObject", async () => {
+    const ret = await client.putObject(TEST_KEY, Buffer.from(TEST_DATA));
+    expect(ret.code).toBe(200);
+    expect(ret.headers.etag).toBeDefined();
+    expect(ret.headers["x-cos-hash-crc64ecma"]).toBeDefined();
+    expect(ret.headers["etag"]).toBeDefined();
+    SHARE.headers = ret.headers;
+    SHARE.md5 = ret.headers["etag"];
+  });
+
+  test("objectMeta", async () => {
+    const ret = await client.objectMeta(TEST_KEY);
+    expect(ret.code).toBe(200);
+    expect(ret.headers.etag).toEqual(SHARE.headers.etag);
+    expect(ret.headers["x-cos-hash-crc64ecma"]).toEqual(SHARE.headers["x-cos-hash-crc64ecma"]);
+    SHARE.headers = ret.headers;
+  });
+
+  test("objectMeta With Agent", async () => {
+    const ret = await clientWithAgent.objectMeta(TEST_KEY);
+    expect(ret.code).toBe(200);
+    expect(ret.headers.etag).toEqual(SHARE.headers.etag);
+    expect(ret.headers["x-cos-hash-crc64ecma"]).toEqual(SHARE.headers["x-cos-hash-crc64ecma"]);
+    SHARE.headers = ret.headers;
+  });
+
+  test("headObject", async () => {
+    const ret = await client.headObject(TEST_KEY);
+    expect(ret.code).toBe(200);
+    expect(ret.headers.etag).toEqual(SHARE.headers.etag);
+    expect(ret.headers["last-modified"]).toEqual(SHARE.headers["last-modified"]);
+    expect(ret.headers["x-cos-hash-crc64ecma"]).toEqual(SHARE.headers["x-cos-hash-crc64ecma"]);
+    expect(ret.headers["etag"]).toEqual(SHARE.md5);
+    SHARE.headers = ret.headers;
+  });
+
+  test("getObject", async () => {
+    const ret = await client.getObject(TEST_KEY);
+    expect(ret.code).toBe(200);
+    expect(ret.headers.etag).toEqual(SHARE.headers.etag);
+    expect(ret.headers["x-cos-hash-crc64ecma"]).toEqual(SHARE.headers["x-cos-hash-crc64ecma"]);
+    expect(ret.headers["etag"]).toEqual(SHARE.md5);
+    expect(ret.buffer!.toString()).toEqual(TEST_DATA);
+  });
+
+  test("getObject With Agent", async () => {
+    const ret = await clientWithAgent.getObject(TEST_KEY);
+    expect(ret.code).toBe(200);
+    expect(ret.headers.etag).toEqual(SHARE.headers.etag);
+    expect(ret.headers["x-cos-hash-crc64ecma"]).toEqual(SHARE.headers["x-cos-hash-crc64ecma"]);
+    expect(ret.headers["etag"]).toEqual(SHARE.md5);
+    expect(ret.buffer!.toString()).toEqual(TEST_DATA);
+  });
+
+  // test("getSignUrl", async () => {
+  //   const url = client.getSignUrl(TEST_KEY);
+  //   const ret = await getFile(url);
+  //   expect(ret.toString()).toEqual(TEST_DATA);
+  // });
+
+  test("deleteObject", async () => {
+    const ret = await client.deleteObject(TEST_KEY);
+    expect(ret.code).toBe(204);
+    const ret2 = await client.headObject(TEST_KEY);
+    expect(ret2.code).toBe(404);
+  });
+
+  // test("putObjectWithUrl", async () => {
+  //   const url = await client.putObjectWithUrl(TEST_KEY, TEST_URL);
+  //   const org = await getFile(url);
+  //   const dis = await getFile(TEST_URL);
+  //   expect(dis).toEqual(org);
+  // });
+});
+
+describe("fix", function() {
+  test("fix: get key with multi-///", () => {
+    (client as any).prefix = undefined;
+    const key = (client as any).getFileKey("//aa/a");
+    expect(key).toEqual("aa/a");
+    (client as any).prefix = process.env.TEST_OSS_PREFEX;
+  });
+
+  test("fix: key with space", async () => {
+    const KEY = "OSS Client.data";
+    const ret = await client.putObject(KEY, Buffer.from(TEST_DATA));
+    expect(ret.code).toBe(200);
+    expect(ret.headers.etag).toBeDefined();
+    expect(ret.headers["x-cos-hash-crc64ecma"]).toBeDefined();
+    expect(ret.headers["etag"]).toBeDefined();
+    const ret2 = await client.deleteObject(KEY);
+    expect(ret2.code).toBe(204);
+    const ret3 = await client.headObject(KEY);
+    expect(ret3.code).toBe(404);
+  });
+});
